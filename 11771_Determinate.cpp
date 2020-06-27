8b122ec7ccf1262aa5122590a7cb8c2a6451a863
ffy062 <56193560+ffy062@users.noreply.github.com>
2020-06-27 13:58:34 +0800

    Add files via upload

---
+++ 11771_Determinate.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+long long int matrix[8][8];
+int valid[8];
+
+// n is the size of the whole matrix, cur is the size of the matrix you wnant to calculate
+long long int det(int n, int cur) {
+    long long int ans = 0;
+    int cnt = 0;
+    // base case
+    if(cur == 2) { // 2 * 2 matrix is easy to calculate 
+        long long int two[2][2];
+        int first = 1;
+
+        for(int i = 0; i < n; ++i) {
+            if(valid[i] == 1) {
+                if(first == 1) {
+                    first = 0;
+                    two[0][0] = matrix[n-cur][i];
+                    two[1][0] = matrix[n-cur+1][i];
+                }
+                else {
+                    two[0][1] = matrix[n-cur][i];
+                    two[1][1] = matrix[n-cur+1][i];
+                }
+            }
+        }
+        ans = two[0][0] * two[1][1] - two[0][1] * two[1][0];
+        
+    }
+    else {
+        for(int i = 0; i < n; ++i) {
+            if(valid[i] == 1) {
+                valid[i] = 0;
+                if(cnt % 2 == 1) {
+                    ans -= matrix[n-cur][i] * det(n, cur - 1);    
+                }
+                else {
+                    ans += matrix[n-cur][i] * det(n, cur - 1);
+                }
+                valid[i] = 1;
+                cnt++;
+            }    
+        }
+    }
+
+    return ans;
+}
+
+
+int main () {
+    int n;
+
+    scanf("%d", &n);
+    for(int i = 0; i < n; ++i) {
+        valid[i] = 1;
+        for(int j = 0; j < n; ++j) {
+            scanf("%lld", &matrix[i][j]);
+        }
+    }
+    long long int ans = det(n, n);
+    printf("%lld\n", ans);
+
+    return 0;
+}
